8b58b6ddf9371cf689627906a0b5e4176f943114
Ale Rimoldi <ale@graphicslab.org>
2015-10-18 11:50:23 +0200

    add testing and option parsing

--- programming-principles-and-practice/06-writing-a-program/exercises/ale/calculator-getline.cpp
+++ programming-principles-and-practice/06-writing-a-program/exercises/ale/calculator-getline.cpp
@@ -1,15 +1,35 @@
 /**
- * this is the calculator the co-learning group produced during the exercises hour
+ * this is a calculator that:
+ * - uses getline() to read a full line at a time
+ * - uses istringstream to get the same behavior as cin on a full string
+ * - directly uses exceptions
  */
 #include <iostream>
 #include <string>
 #include <exception>
 #include <sstream>
+#include <vector>
+#include "optionparser.h"
+#define CATCH_CONFIG_RUNNER
+#include "catch.hpp"
 using namespace std;
 
 void error(string message) {
     cout << message << "\n";
 }
+
+enum  optionIndex { UNKNOWN, HELP, TEST };
+const option::Descriptor usage[] =
+{
+    {UNKNOWN, 0,"" , "",option::Arg::None, "USAGE: alculator [options]\n\n"
+        "Options:" },
+    {HELP, 0,"h", "help",option::Arg::None, "  --help, -h  \tPrint usage and exit." },
+    {TEST, 0,"", "test",option::Arg::None, "  --test       \tRun the tests." },
+    {UNKNOWN, 0, "", "",option::Arg::None, "\nExamples:\n"
+        "  calculator --test \n"
+        "  calculator\n" },
+    {0,0,0,0,0,0}
+};
 
 class Token {
     public:
@@ -76,12 +96,37 @@ double primary();
 
 Token_stream ts;
 
-int main() {
+int main(int argc, char* argv[]) {
+
+    argc-=(argc>0); argv+=(argc>0); // skip program name argv[0] if present
+    option::Stats  stats(usage, argc, argv);
+    std::vector<option::Option> options(stats.options_max);
+    std::vector<option::Option> buffer(stats.buffer_max);
+    option::Parser parse(usage, argc, argv, &options[0], &buffer[0]);
+
+    if (parse.error())
+        return 1;
+
+    if (options[HELP]/* || argc == 0*/) {
+        option::printUsage(std::cout, usage);
+        return 0;
+    }
+    if (options[TEST]) {
+        int result = Catch::Session().run( argc, argv );
+        return result;
+    }
+
+
+    for (option::Option* opt = options[UNKNOWN]; opt; opt = opt->next()) {
+        std::cout << "Unknown option: " << opt->name << "\n";
+    }
+
+
     string calculation;
     double result;
     cout << "Please enter an expression (RETURN to quit) :" << endl;
     while (getline(cin, calculation))
-    {
+   {
         // -3+2
         // -3 + 2
         // 3+(-2*3)
@@ -94,7 +139,6 @@ int main() {
 
 double calculate(string calculation) {
     double result;
-    istringstream cs(calculation);
     ts.set(calculation);
     result = expression();
     return result;
@@ -165,6 +209,12 @@ double primary()
         default:
             throw std::invalid_argument("primary expected");
     }
+}
+
+TEST_CASE( "r", "[factorial]" ) {
+    REQUIRE( calculate("4") == 4 );
+    REQUIRE( calculate("4+2") == 6 );
+    // REQUIRE( calculate("4*2") == 8 );
 }
 
 /**
