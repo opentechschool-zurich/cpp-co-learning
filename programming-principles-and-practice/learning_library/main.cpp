388a239291f49fbba206949fd2552b9680117bd6
ale rimoldi <ale.comp_06@xox.ch>
2015-11-19 15:36:28 +0100

    restore parisa's files as she commited them

--- programming-principles-and-practice/learning_library/main.cpp
+++ programming-principles-and-practice/learning_library/main.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
-// #include "four_op.hpp"
-// #include "area.hpp"
-#include "sq_area.cpp"
+#include "four_op.hpp"
+#include "area.hpp"
 
 using namespace std;
 
 int main(){
-  cout<<sq_area(5.0)<<endl;
-  // cout<<circ_area(2)<<endl;
-  // cout<<rec_area(5,6)<<endl;
-  // cout<<tri_area(5,6)<<endl;
+  cout<<sq_area(5)<<endl;
+  cout<<circ_area(2)<<endl;
+  cout<<rec_area(5,6)<<endl;
+  cout<<tri_area(5,6)<<endl;
 }
--- programming-principles-and-practice/learning_library/sq_area.cpp
+++ programming-principles-and-practice/learning_library/sq_area.cpp
@@ -1,5 +1,5 @@
 #include"four_op.hpp"
-// #include"area.hpp"
+#include"area.hpp"
 
 double sq_area(double x){
   return multiply(x,x);
