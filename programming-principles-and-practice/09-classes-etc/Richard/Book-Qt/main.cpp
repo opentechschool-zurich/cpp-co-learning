e9e5f3afcc228cb155a74e9e41f8a6796e4ae700
richardeigenmann <richard.eigenmann@gmail.com>
2015-11-23 23:23:35 +0100

    Cleanup

--- programming-principles-and-practice/09-classes-etc/Richard/Book-Qt/main.cpp
+++ programming-principles-and-practice/09-classes-etc/Richard/Book-Qt/main.cpp
@@ -1,4 +1,17 @@
-//  #include "stdafx.h" 
+/*
+Chapter 9 Exercise 5
+5. This exercise and the next few require you to design and implement
+a Book class, such as you can imagine as part of software for a library.
+Class Book should have members for the ISBN, title, author, and copyright
+date. Also store data on whether or not the book is checked out. Create
+functions for returning those data values. Create functions for checking
+a book in and out. Do simple validation of data entered into a Book; for
+example, accept ISBNs only of the form n-n-n-x where n is an integer and x
+is a digit or a letter. Store an ISBN as a string.
+*/
+
+
+//  #include "stdafx.h"
 #include <string>
 #include <iostream>
 //#include <regex>
@@ -145,4 +158,3 @@ int main(int argc, char *argv[]) {
 
     //return 0;
 }
-
