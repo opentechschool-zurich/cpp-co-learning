2917137b7704b862c8e56319470b5f4fea78b8bf
Ale Rimoldi <ale@graphicslab.org>
2015-12-05 14:52:32 +0100

    qt sample app showing an empty window

---
+++ programming-principles-and-practice/12-display-model/ale/00-triangle/mainWindow.cpp
@@ -0,0 +1,7 @@
+#include "mainWindow.h"
+MainWindow::MainWindow(QWidget *parent, Qt::WindowFlags flags)
+    : QMainWindow(parent, flags)
+{
+    setObjectName("MainWindow");
+    setWindowTitle("Qt Main Window Example");
+}
