6bba83a0026aac1fbe5c32272071f8105ebee6dd
Ale Rimoldi <ale@graphicslab.org>
2015-12-09 00:40:32 +0100

    getting the example from 12.3 to compile and run... there are many errors in the provided files

--- programming-principles-and-practice/12-display-model/ale/00-window-graph/README.md
+++ programming-principles-and-practice/12-display-model/ale/00-window-graph/README.md
@@ -1,5 +1,11 @@
-On Debian you need to install
+# Windows and Graph helper classes
+
+This is the first example in section 12.3 with all the "helper" files by Stroustrup modified so that they compile: there are many errors, most of all in the `.h` files!
+
+This [Programming Principle and Practice Forum thread](https://groups.google.com/forum/#!topic/ppp-public/BtlzdWGuQpQ) gives some clues on how to fix the files.
+
+On Debian/Ubuntu you need to install
 
 - libftlk1.3-dev
 
-you get `fluid` a Gui builder for fltk
+you get `fluid` a Gui builder for fltk.
--- programming-principles-and-practice/12-display-model/ale/00-window-graph/Simple_window.h
+++ programming-principles-and-practice/12-display-model/ale/00-window-graph/Simple_window.h
@@ -5,32 +5,18 @@ using namespace Graph_lib;
 // Simple_window is basic scaffolding for ultra-simple interaction with graphics
 // it provides one window with one "next" button for ultra-simple animation
 
-struct Simple_window : Window {
-	Simple_window(Point xy, int w, int h, const string& title )
-	: Window(xy,w,h,title),
-	  button_pushed(false),
-	  next_button(Point(x_max()-70,0), 70, 20, "Next", cb_next) { attach(next_button); }
+struct Simple_window : Graph_lib::Window {
+	Simple_window(Point xy, int w, int h, const string& title );
 	
-	void wait_for_button()
-	// modified event loop
-	// handle all events (as per default), but quit when button_pushed becomes true
-	// this allows graphics without control inversion
-	{
-		while (!button_pushed) Fl::wait();
-		button_pushed = false;
-		Fl::redraw();
-	}
+	bool wait_for_button();
 
 	Button next_button;
 private:
 	bool button_pushed;
 	
-	static void cb_next(Address, Address addr) // callback for next_button
+	static void cb_next(Address, Address addr); // callback for next_button
 	//	{ reference_to<Simple_window>(addr).next(); }
-	{
-		static_cast<Simple_window*>(addr)->next();
-	}
 
-	void next() { button_pushed = true; }
+	void next();
 
 };
