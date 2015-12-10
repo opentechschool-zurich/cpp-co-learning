1290293dd5ff0d02c86f258de98ad49f7ebb8f60
Ale Rimoldi <ale@graphicslab.org>
2015-12-10 20:33:31 +0100

    add the ellipses from 13.3

--- programming-principles-and-practice/13-graphics-classes/ale/00-circles/mainWindow.cpp
+++ programming-principles-and-practice/13-graphics-classes/ale/00-circles/mainWindow.cpp
@@ -14,4 +14,8 @@ void MainWindow::paintEvent(QPaintEvent* /* event */)
     painter.drawEllipse(QPoint(100, 100), 20, 20);
     painter.drawEllipse(QPoint(120, 100), 40, 40);
     painter.drawEllipse(QPoint(140, 100), 60, 60);
+
+    painter.drawEllipse(QPoint(340, 100), 60, 80);
+    painter.drawEllipse(QPoint(340, 100), 60, 30);
+    painter.drawEllipse(QPoint(340, 100), 30, 30);
 }
