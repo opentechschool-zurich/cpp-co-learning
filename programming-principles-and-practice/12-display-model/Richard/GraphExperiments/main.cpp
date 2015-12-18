a94f985e974716a348192dfbe01846aeb19bd669
Richard Eigenmann <richard.eigenmann@gmail.com>
2015-12-19 00:55:05 +0100

    GraphExperiments

--- programming-principles-and-practice/12-display-model/Richard/GraphExperiments/main.cpp
+++ programming-principles-and-practice/12-display-model/Richard/GraphExperiments/main.cpp
@@ -17,6 +17,7 @@ int main(int argc, char **argv) {
     poly.add(Point{350, 100}); // add another point
     poly.add(Point{400, 200}); // add a third point
     poly.set_color(Color::red); // adjust properties of poly
+    poly.set_style(Line_style(Line_style::dash, 4));
     win.attach(poly); // connect poly to the window
 
     Graph_lib::Polygon poly2;
@@ -25,6 +26,7 @@ int main(int argc, char **argv) {
     poly2.add(Point{80, 280}); // add a third point
     poly2.add(Point{20, 200}); // add a third point
     poly2.set_color(Color::dark_cyan); // adjust properties of poly
+    poly2.set_style(Line_style::dash);
     //poly2.fill_color(Color::dark_cyan);
     win.attach(poly2); // connect poly to the window
 
@@ -41,8 +43,50 @@ int main(int argc, char **argv) {
     win.set_label("Canvas #3");
 
 
-    Function sine {sin,0,100,Point{20,150},1000,50,50};
+    Function sine{sin, 0, 100, Point
+        {20, 150}, 1000, 50, 50};
     win.attach(sine);
+
+    Rectangle r{Point
+        {200, 200}, 100, 50};
+    r.set_fill_color(Color::yellow);
+    win.attach(r);
+
+    Closed_polyline poly_rect;
+    poly_rect.add(Point{100, 50});
+    poly_rect.add(Point{200, 50});
+    poly_rect.add(Point{200, 100});
+    poly_rect.add(Point{100, 100});
+    poly_rect.add(Point{50, 75});
+    poly_rect.set_style(Line_style(Line_style::dash, 2));
+    poly_rect.set_fill_color(Color::green);
+    win.attach(poly_rect);
+
+    Text t{Point
+        {150, 150}, "Hello, graphical world!"};
+    //t.set_font(Font::times_bold);
+    t.set_font_size(20);
+    win.attach(t);
+    
+    Image ii {Point{330,250},"image.jpg"};
+    win.attach(ii);
+
+    Circle c {Point{100,200},50};
+    win.attach(c);
+    
+    Ellipse e {Point{100,200}, 75,25};
+    e.set_color(Color::dark_red);
+    win.attach(e);
+    
+    Mark m {Point{100,200},'x'};
+    win.attach(m);
+
+    ostringstream oss;
+    oss << "screen size: " << x_max() << "*" << y_max()
+    << "; window size: " << win.x_max() << "*" << win.y_max();
+    Text sizes {Point{100,20},oss.str()};
+    win.attach(sizes);
+    
     
     Fl_Window *window = new Fl_Window(340, 180);
     Fl_Box *box = new Fl_Box(20, 40, 300, 100, "Hello, World!");
