ed21a03ed9d36bf73c606a1dbca8e6a0c8f7efb7
Ale Rimoldi <ale@graphicslab.org>
2016-01-19 19:46:44 +0100

    first working my myproject

---
+++ programming-principles-and-practice/12-display-model/ale/myproject/firstexample.cpp
@@ -0,0 +1,16 @@
+#include "graphicscode/Simple_window.h" // window library 
+#include "graphicscode/Graph.h"
+
+int main()
+{
+  using namespace Graph_lib;
+
+  Point tl {100, 100};
+
+  Simple_window win {tl, 600, 400, "My window"};
+
+  win.wait_for_button();// give control to the display engine and thus
+                        // display the window on screen 
+}
+
+ 
