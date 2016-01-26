a1b6ea4deb254a45b2c214c1226d3db5182ce17b
Ale Rimoldi <ale@graphicslab.org>
2016-01-26 19:35:03 +0100

    remove height from square

--- programming-principles-and-practice/12-display-model/ale/presentation/01-square/square.cpp
+++ programming-principles-and-practice/12-display-model/ale/presentation/01-square/square.cpp
@@ -7,23 +7,20 @@ class Square
 public:
     Square() {};
     void setWidth(const int w) {width = w;};
-    void setHeight(const int h) {height = h;};
     int getArea() const;
 private:
     int width{0};
-    int height{0};
 };
 
 int Square::getArea() const
 {
-    return height * width;
+    return width * width;
 }
 
 int main()
 {
     Square square{};
     square.setWidth(10);
-    square.setHeight(5);
     cout << "Area: " << square.getArea() << endl;
     return 0;
 }
