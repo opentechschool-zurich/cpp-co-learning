437c5261d1dcd90dd693227f938002bdd090bd93
Ale Rimoldi <ale@graphicslab.org>
2016-02-03 21:33:15 +0100

    complete the colors menu; hide and show it

--- programming-principles-and-practice/15-graphing/ale/16.7/Menues_window.cpp
+++ programming-principles-and-practice/15-graphing/ale/16.7/Menues_window.cpp
@@ -6,10 +6,17 @@ using namespace Graph_lib;
 
 Menues_window::Menues_window(Point xy, int w, int h, const string& title)
     :Window(xy, w, h, title),
+    menu_button(Point(x_max()-80,30), 80, 20, "color menu", [](Address, Address w){reference_to<Menues_window>(w).menu_pressed();}),
     color_menu{Point{x_max()-70, 40}, 70, 20, Menu::vertical, "color"},
 	circle{ Point{ 100,100 }, 100 }
 {
 	attach(circle);
+
+	attach(menu_button);
+
     color_menu.attach(new Button{Point{0,0}, 0, 0, "red", [](Address, Address w) {reference_to<Menues_window>(w).red_pressed();}});
+    color_menu.attach(new Button{Point{0,0}, 0, 0, "blue", [](Address, Address w) {reference_to<Menues_window>(w).blue_pressed();}});
+    color_menu.attach(new Button{Point{0,0}, 0, 0, "black", [](Address, Address w) {reference_to<Menues_window>(w).black_pressed();}});
     attach(color_menu);
+    color_menu.hide();
 }
--- programming-principles-and-practice/15-graphing/ale/16.7/Menues_window.h
+++ programming-principles-and-practice/15-graphing/ale/16.7/Menues_window.h
@@ -10,13 +10,16 @@ class Menues_window : Window
 public:
 	Menues_window(Point xy, int w, int h, const string& title);
 
+    Button menu_button;
     Menu color_menu;
     Circle circle;
 
 	// the actions:
-	void red_pressed() { change(Color::red); }
-	void blue_pressed() { change(Color::blue); }
-	void black_pressed() { change(Color::black); }
+	void menu_pressed() { menu_button.hide(); color_menu.show(); }
+	void menu_hide() { menu_button.show(); color_menu.hide(); }
+	void red_pressed() { change(Color::red); menu_hide(); }
+	void blue_pressed() { change(Color::blue); menu_hide(); }
+	void black_pressed() { change(Color::black); menu_hide(); }
 	void change(Color c) {circle.set_color(c); redraw();}
 };
 
