9e46f4e74807f96fd14e88d24c35855f795244df
Sondermann <sonder.mann@posteo.org>
2016-02-07 18:28:43 +0100

    Add drill 5.

--- programming-principles-and-practice/17-vector-free-store/christian/drill.cpp
+++ programming-principles-and-practice/17-vector-free-store/christian/drill.cpp
@@ -2,6 +2,13 @@
 
 using namespace std;
 
+void print_array10(ostream& os, int* a){
+  for (int i = 0; i < 10; ++i)
+    os << a[i] << endl; 
+
+ }
+
+
 int main()
 {
   int* myptr = new int[10];
@@ -9,7 +16,16 @@ int main()
   for (int i = 0; i < 10; ++i)
       cout << myptr[i] << endl; 
 
+  print_array10(cout, myptr);
+
   delete[] myptr; 
+
+  int* myptr2 = new int[11] {100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110}; 
+
+  for (int i = 0; i < 11; ++i)
+      cout << myptr2[i] << endl; 
+
+
 
   return 0;
 }
