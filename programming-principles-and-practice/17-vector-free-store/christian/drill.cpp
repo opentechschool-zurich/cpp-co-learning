d869347257092485ea3436e8c35509d22bc2e4f7
Sondermann <sonder.mann@posteo.org>
2016-02-07 18:18:32 +0100

    Do drill 3.

--- programming-principles-and-practice/17-vector-free-store/christian/drill.cpp
+++ programming-principles-and-practice/17-vector-free-store/christian/drill.cpp
@@ -1,11 +1,15 @@
+#include<iostream>
+
 using namespace std;
 
 int main()
 {
   int* myptr = new int[10];
 
+  for (int i = 0; i < 10; ++i)
+      cout << myptr[i] << endl; 
 
-
+  delete[] myptr; 
 
   return 0;
 }
