075a08af54dcfc5dd108382a4e6c4fefbd602aea
Ale Rimoldi <ale@graphicslab.org>
2016-02-09 21:13:49 +0100

    remove two build directories that should have not been there...

--- programming-principles-and-practice/13-graphics-classes/ale/00-grid/build/CMakeFiles/grid_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "grid_automoc.cpp"
-  "CMakeFiles/grid_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang)
-  include(CMakeFiles/grid_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
