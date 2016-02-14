40768403fff3ea79e5477b3f95d3c981b7fb77d1
Christian Kascha <christian.kascha@posteo.de>
2016-02-14 11:14:39 +0100

    Push all files.

---
+++ programming-principles-and-practice/18-vectors-arrays/christian/vortrag/slide_config.js
@@ -0,0 +1,26 @@
+var SLIDE_CONFIG = {
+   // Slide Settings
+   settings: {
+     title: 'Vectors and Arrays', 
+     subtitle: 'Lift-off by overcoming the constraints of the machine! :-)', 
+     useBuilds: true, // Default: true. False will turn off slide animation builds. 
+     usePrettify: true, // Default: true 
+     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+     favIcon: 'images/emacs-icon.png', 
+     fonts: [
+       'Open Sans:regular,semibold,italic,italicsemibold',
+       'Source Code Pro'
+     ],
+   }, 
+ 
+   // Author information
+   presenters: [{
+     name: '', 
+     company: '', 
+     gplus: '', 
+     twitter: '', 
+     www: '', 
+     github: '', 
+   }]
+};
