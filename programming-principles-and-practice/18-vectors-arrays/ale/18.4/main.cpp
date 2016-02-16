fc6f98a9f7191785d6a2ba46d137ca33cd68210a
Ale Rimoldi <ale@graphicslab.org>
2016-02-16 20:04:07 +0100

    fix and make beatyful the x class

--- programming-principles-and-practice/18-vectors-arrays/ale/18.4/main.cpp
+++ programming-principles-and-practice/18-vectors-arrays/ale/18.4/main.cpp
@@ -8,7 +8,7 @@ class X {
 public:
     int val;
     void out(const string& s, int nv)
-        {cerr << this << "->" << s << ":" << val << "(" << ")" << endl;}
+        {cerr << this << "->" << s << ":" << val << "(" << nv << ")" << endl;}
     X()
         {out("X()", 0); val = 0;};
     X(int v)
@@ -16,9 +16,9 @@ public:
     X(const X& x)
         {val = x.val; out("X(X&)", x.val);};
     X& operator=(const X& a)
-        {out("X(X&)", a.val); val = a.val; return *this;};
+        {out("X& X(X&)", a.val); val = a.val; return *this;};
     ~X()
-        {out("~X()", 0);};
+        {out("~X()", val);};
 };
 
 X glob(2);
@@ -34,18 +34,32 @@ int main()
 {
     cout << "main()" << endl;
     X loc{4};
+    cout << "..." << endl;
     X loc2{loc};
+    cout << "..." << endl;
     loc = X{5};
+    cout << "..." << endl;
     loc2 = copy(loc);
+    cout << "..." << endl;
     loc2 = copy2(loc);
+    cout << "..." << endl;
     X loc3{6};
+    cout << "..." << endl;
     X&r = ref_to(loc);
+    cout << "..." << endl;
     delete make(7);
+    cout << "..." << endl;
     delete make(8);
+    cout << "..." << endl;
     vector<X> v(4);
+    cout << "..." << endl;
     XX loc4;
+    cout << "..." << endl;
     X* p = new X{9};
+    cout << "..." << endl;
     delete p;
+    cout << "..." << endl;
     X* pp = new X[5];
+    cout << "..." << endl;
     delete[] pp;
 }
