e4fe69de5061bd42d157f458bd90c1b0db0dba4b
Sondermann <sonder.mann@posteo.org>
2016-02-28 19:50:50 +0100

    20.1.1 working with data

--- programming-principles-and-practice/20-containers-iterators/christian/chapter/main.cpp
+++ programming-principles-and-practice/20-containers-iterators/christian/chapter/main.cpp
@@ -1,5 +1,6 @@
 // main.cpp
 #include<vector>
+#include<iostream>
 
 using namespace std;
 
@@ -12,8 +13,28 @@ void fct(){
   int jack_count = 0;
   double* jack_data = get_from_jack(&jack_count);
   vector<double>* jill_data = get_from_jill();
-  // ... process ...
-  //
+
+  double h = -1;
+  double* jack_high;
+  double* jill_high;
+  for (int i = 0; i<jack_count; ++i)
+    if (h<jack_data[i])
+      {
+        jack_high = &jack_data[i];
+        h = jack_data[i]; 
+      }
+
+  h = -1;
+  for (int i = 0; i<jill_data->size(); ++i)
+    if (h<(*jill_data)[i]){
+      jill_high = &(*jill_data)[i];
+      h = (*jill_data)[i]; 
+    }
+
+  cout << "Jill's max: " << *jill_high << endl
+       << "Jack's max: " << *jack_high << endl;
+
+
   delete[] jack_data;
   delete jill_data; 
   
