87f73e8b9521f02c9097ca21847717dde8baba73
Sondermann <sonder.mann@posteo.org>
2016-02-28 19:42:31 +0100

    20.1 Storgin and processing data

--- programming-principles-and-practice/20-containers-iterators/christian/chapter/main.cpp
+++ programming-principles-and-practice/20-containers-iterators/christian/chapter/main.cpp
@@ -1,1 +1,31 @@
 // main.cpp
+#include<vector>
+
+using namespace std;
+
+
+double* get_from_jack(int* count){}
+
+vector<double>* get_from_jill(){}
+
+void fct(){
+  int jack_count = 0;
+  double* jack_data = get_from_jack(&jack_count);
+  vector<double>* jill_data = get_from_jill();
+  // ... process ...
+  //
+  delete[] jack_data;
+  delete jill_data; 
+  
+}
+
+
+int main(int argc, char *argv[])
+{
+    
+
+    return 0;
+}
+
+
+
