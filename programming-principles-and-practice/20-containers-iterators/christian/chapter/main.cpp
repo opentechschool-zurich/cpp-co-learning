9271ffcd217a6b0a512b85146c20f8ee63e3db47
Sondermann <sonder.mann@posteo.org>
2016-02-28 20:09:14 +0100

    20.1.2 Generalizing code

--- programming-principles-and-practice/20-containers-iterators/christian/chapter/main.cpp
+++ programming-principles-and-practice/20-containers-iterators/christian/chapter/main.cpp
@@ -9,27 +9,27 @@ double* get_from_jack(int* count){}
 
 vector<double>* get_from_jill(){}
 
+double* high(double* first, double* last){
+  double h = -1;
+  double* high;
+  for (double* p =first; p!=last; ++p)
+      if (h < *p){high = p; h = *p;}
+
+  return high; 
+}
+
+
+
 void fct(){
   int jack_count = 0;
   double* jack_data = get_from_jack(&jack_count);
   vector<double>* jill_data = get_from_jill();
 
-  double h = -1;
-  double* jack_high;
-  double* jill_high;
-  for (int i = 0; i<jack_count; ++i)
-    if (h<jack_data[i])
-      {
-        jack_high = &jack_data[i];
-        h = jack_data[i]; 
-      }
+  double* jack_high = high(jack_data, jack_data+jack_count);
+  
 
-  h = -1;
-  for (int i = 0; i<jill_data->size(); ++i)
-    if (h<(*jill_data)[i]){
-      jill_high = &(*jill_data)[i];
-      h = (*jill_data)[i]; 
-    }
+  vector<double>& v = *jill_data; 
+  double* jill_high = high(&v[0], &v[0]+v.size()); 
 
   cout << "Jill's max: " << *jill_high << endl
        << "Jack's max: " << *jack_high << endl;
