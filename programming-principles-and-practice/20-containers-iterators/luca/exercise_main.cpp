fbe040d76dc46c46d6d96a7dfe723f4de49de214
Luca Marchesi <skiat@skiat.ch>
2016-03-02 23:10:31 +0100

    code cleanup and quick improvement to have a single line which does the insertion

--- programming-principles-and-practice/20-containers-iterators/luca/exercise_main.cpp
+++ programming-principles-and-practice/20-containers-iterators/luca/exercise_main.cpp
@@ -11,38 +11,33 @@ template<typename T>
 void do_container(int n) {
     T myvect;
     
-    int myrand = rand() % n;
-    myvect.push_back(myrand);
-    
     for (int i = 0; i<n; i++) { // timing loop
-      myrand = rand() % n;
-      
-      // loop all the vector looking for the element to insert before it
-      auto it = myvect.begin();
-      for ( ;it!=myvect.end(); ++it) {
-	if (*it > myrand) {
-	  // once found insert and exit the loop
-	  myvect.insert(it, myrand);
-	  break;
-	}
-      }
-      // if we are here and iterator is at the end
-      // it means we did not insert anything yet, so push at the end
-      if ( it == myvect.end() )
-	myvect.push_back(myrand);
-      
-      
+        int myrand = rand() % n;
+
+        // loop all the vector looking for the element to insert before it
+        auto it = myvect.begin();
+        for (; it!=myvect.end(); ++it) {
+            if (*it > myrand) { // once found insert and exit the loop
+                break;
+            }
+        }
+        // when the vector is empty we have begin()=end() so insert before end() will do the trick
+        // when the vector has an entry with a bigger value the iterator is at the correct position
+        // when the vector has nothing with a bigger value then myrand we will fully loop the vector
+        // // the iterator will be at end() and it is also good
+        myvect.insert(it, myrand);
     }
     
-//    for (auto val : myvect) cout << val << " " ;
-  
+    //for (auto val : myvect) cout << val << " " ;
+    //cout << myvect.size();
+
 }
 
 
 int main() {
     // just initialize random to something..
     srand(114552);
-      
+
     // decide how many entries to generate
     int n = 100000;
 
@@ -51,13 +46,13 @@ int main() {
     auto endTime = system_clock::now();
     auto ms = duration_cast<milliseconds>( endTime - startTime).count();
     cout << "do_container<vector>() inserting " << n << " times took "
-        << ms << "ms\n";
+         << ms << "ms\n";
 
     startTime = system_clock::now();
     do_container< list<int> >(n);
     endTime = system_clock::now();
     ms = duration_cast<milliseconds>( endTime - startTime).count();
     cout << "do_container<list>() inserting " << n << " times took "
-        << ms << "ms" << endl;
+         << ms << "ms" << endl;
 }
 
