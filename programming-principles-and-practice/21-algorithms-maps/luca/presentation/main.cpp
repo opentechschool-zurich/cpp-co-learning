f6e1a4ce861c1c7939a0a226fa9f641730f6d8ba
Luca Marchesi <skiat@skiat.ch>
2016-03-07 23:49:19 +0100

    added accumulator example

--- programming-principles-and-practice/21-algorithms-maps/luca/presentation/main.cpp
+++ programming-principles-and-practice/21-algorithms-maps/luca/presentation/main.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-int x;
+int x{0}; // needs to be global, because initialized later
 bool isBiggerThan_x (int i) {
   return i > x;
 }
@@ -29,23 +29,33 @@ int main () {
 
     vector<int> myvector {1, 2, 3, 4};
 
-    // with global function
+    // Predicate with global function
     x = 2;
     auto it = find_if (myvector.begin(), myvector.end(), isBiggerThan_x);
     if ( it != myvector.end() ) {
         cout << "first result with global function isBiggerThan_x x=2     is: " << *it << endl;
     }
 
-    // with function object
+    // Predicate with function object
     it = find_if (myvector.begin(), myvector.end(), myDynamicBiggerThan(3));
     if ( it != myvector.end() ) {
         cout << "first result with function object myDynamicBiggerThan(3) is: " << *it << endl;
     }
 
-    // with lambda function
+    // Predicate with lambda function
     int bigger = 1;
     it = find_if (myvector.begin(), myvector.end(), [=](int i){ return i > bigger; });
     if ( it != myvector.end() ) {
         cout << "first result with lambda function using the var bigger=1 is: " << *it << endl;
     }
+
+    // strange accumulator
+    cout << "strange accumulation" << endl;
+    cout << "is: " << accumulate(myvector.begin(), myvector.end(), 0, [](int init, int elem){
+        cout << "init: " << init << " - elem:" << elem << endl;
+        return init; // + elem; // try doing nothing
+    }) << endl;
+
+
+    return 0;
 }
