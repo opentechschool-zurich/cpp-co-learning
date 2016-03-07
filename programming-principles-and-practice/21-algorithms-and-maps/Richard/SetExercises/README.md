c76fb8ea705f8bd923a295327b6df9a2cf10e7dc
Richard Eigenmann <richard.eigenmann@gmail.com>
2016-03-08 00:38:19 +0100

    SetExercises

---
+++ programming-principles-and-practice/21-algorithms-and-maps/Richard/SetExercises/README.md
@@ -0,0 +1,45 @@
+# Sample code for sets
+
+# To Run:
+
+``` bash
+git clone git@github.com:opentechschool-zurich/cpp-co-learning.git
+cd programming-principles-and-practice/21-algorithms-and-maps/Richard/SetExercises
+mkdir build
+cd build
+cmake ..
+make
+./SetExercises
+```
+
+# Results
+```
+Here is the Zoo:
+Animal: Sid Species: Snake Legs: 0
+Animal: Simba Species: Lion Legs: 4
+Animal: Tim Species: Dog Legs: 4
+Animal: Fido Species: Dog Legs: 4
+Animal: King Kong Species: Gorilla Legs: 2
+Animal: Mikey Mouse Species: Mouse Legs: 2
+Animal: Dolly Species: Sheep Legs: 4
+
+A function object to find_if to find King Kong in the Zoo:
+We found King Kong: Animal: King Kong Species: Gorilla Legs: 2
+
+A lambda find_if to find the lion in the Zoo:
+We found the lion: Animal: Simba Species: Lion Legs: 4
+
+Our zoo has 20 legs
+
+Creating a map from our zoo:                                                           Animal Fido not inserted because animal Animal: Tim Species: Dog Legs: 4                     already has the key
+zooMap contains:                                                                                              Key: Dog        Animal: Tim Species: Dog Legs: 4
+Key: Gorilla    Animal: King Kong Species: Gorilla Legs: 2
+Key: Lion       Animal: Simba Species: Lion Legs: 4                                                                      Key: Mouse      Animal: Mikey Mouse Species: Mouse Legs: 2                                                               Key: Sheep      Animal: Dolly Species: Sheep Legs: 4                                                                     Key: Snake      Animal: Sid Species: Snake Legs: 0
+
+Creating a multipmap from our zoo:                                                                                       zooMultimap contains:                                                                                                                  Key: Dog        Animal: Tim Species: Dog Legs: 4                                                                                       Key: Dog        Animal: Fido Species: Dog Legs: 4
+Key: Gorilla    Animal: King Kong Species: Gorilla Legs: 2
+Key: Lion       Animal: Simba Species: Lion Legs: 4
+Key: Mouse      Animal: Mikey Mouse Species: Mouse Legs: 2
+Key: Sheep      Animal: Dolly Species: Sheep Legs: 4
+Key: Snake      Animal: Sid Species: Snake Legs: 0
+```
---
+++ programming-principles-and-practice/21-algorithms-and-maps/Richard/SetExercises/main.cpp
@@ -0,0 +1,114 @@
+#include <list>
+#include <iostream>
+#include <algorithm>
+#include <map>
+
+using namespace std;
+
+// create a class for an Animal
+struct Animal {
+public:
+    string species;
+    string name;
+    int legs;
+};
+
+// override the << operator so that we can just cout << animal
+ostream& operator<<(ostream& os, const Animal& referencedAnimal)
+{
+    os << "Animal: " << referencedAnimal.name
+        << " Species: " << referencedAnimal.species
+        << " Legs: " << referencedAnimal.legs << endl;
+    return os;
+}
+
+// a Function object that compares an animal name
+class MatchAnimalName {
+    string  storedNameToMatch;
+public:
+    // store the argument
+    MatchAnimalName( string nameToMatch ) : storedNameToMatch( nameToMatch ) { }
+    bool operator()( Animal x ) const {
+        return x.name == storedNameToMatch;
+    }
+};
+
+
+// accumulator function for legs
+int legsAccumulator(int priorCount, const Animal& a) {
+    return priorCount + a.legs;
+}
+
+
+int main() {
+    list<Animal> Zoo {
+        {"Snake", "Sid", 0},
+        {"Lion", "Simba", 4},
+        {"Dog", "Tim", 4},
+        {"Dog", "Fido", 4},
+        {"Gorilla", "King Kong", 2},
+        {"Mouse", "Mikey Mouse", 2},
+        {"Sheep", "Dolly", 4}
+    };
+
+    cout << "Here is the Zoo:" << endl;
+    for (auto animal : Zoo) {
+        cout << animal;
+    }
+
+    cout << endl << "A function object to find_if to find King Kong in the Zoo:" << endl;
+    auto foundIterator = find_if( Zoo.begin(), Zoo.end(), MatchAnimalName("King Kong") );
+    if ( foundIterator != Zoo.end() ) {
+        cout << "We found King Kong: " << *foundIterator;
+    }else {
+        cout << "We did not find King Kong." << endl;
+    }
+
+
+     cout << endl << "A lambda find_if to find the lion in the Zoo:" << endl;
+     auto foundLionIterator = find_if( Zoo.begin(), Zoo.end(),
+        [](const Animal& a) -> bool { return a.species == "Lion"; } );
+     if ( foundLionIterator != Zoo.end() ) {
+         cout << "We found the lion: " << *foundLionIterator;
+     } else {
+         cout << "We did not find the lion." << endl;
+    }
+
+
+    cout << endl << "Our zoo has "
+        << accumulate( Zoo.begin(), Zoo.end(), 0, legsAccumulator)
+        << " legs" << endl << endl;
+
+
+    cout << "Creating a map from our zoo: " << endl;
+    map<string, Animal> zooMap;
+    // manually copy the animals into the map with the key as species:
+    for ( auto animal : Zoo ) {
+        auto returnIterator = zooMap.insert( pair<string,Animal> (animal.species, animal) );
+        if ( returnIterator.second == false ) {
+            cout << "Animal " << animal.name << " not inserted because animal "
+                << returnIterator.first->second
+                << "already has the key" << endl;
+        }
+    }
+    cout << endl << "zooMap contains:" << endl;
+    for ( auto element : zooMap ) {
+        cout << "Key: " << element.first << "\t" << element.second;
+    }
+
+
+    cout << endl << "Creating a multipmap from our zoo: " << endl;
+    multimap<string, Animal> zooMultimap;
+    // manually copy the animals into the map with the key as species:
+    for ( auto animal : Zoo ) {
+        auto returnIterator = zooMultimap.insert( pair<string,Animal> (animal.species, animal) );
+    }
+    cout << "zooMultimap contains:" << endl;
+    for ( auto element : zooMultimap ) {
+        cout << "Key: " << element.first << "\t" << element.second;
+    }
+
+
+
+    return 0;
+}
