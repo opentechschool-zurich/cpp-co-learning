f0c1620ae46244037a609622f3f5beb8e6d3aca9
Richard Eigenmann <richard.eigenmann@gmail.com>
2016-03-27 13:20:45 +0200

    Updating Regex Presentation

--- programming-principles-and-practice/09-classes-etc/Richard/ISBNPatttern/main.cpp
+++ programming-principles-and-practice/09-classes-etc/Richard/ISBNPatttern/main.cpp
@@ -1,15 +1,15 @@
 #include <boost/regex.hpp>
 // remember to include the file /usr/lib64/libboost_regex.so in the additional linker files
-
+using namespace boost;
 using namespace std;
 
 int main() {
-    boost::regex ISBNPattern{ R"(^\d+-\d+-\d+-\d+-\d$)" };
+    regex ISBNPattern{ R"(^\d+-\d+-\d+-\d+-\d$)" };
     string isbn1 = "978-0-321-99278-9";
     string isbn2 = "978-0-321-99278";
     cout << isbn1 << " regex_match "
-            << boost::regex_match(isbn1, ISBNPattern) << '\n';
+            << regex_match(isbn1, ISBNPattern) << '\n';
     cout << isbn2 << " regex_match "
-            << boost::regex_match(isbn2, ISBNPattern) << '\n';
+            << regex_match(isbn2, ISBNPattern) << '\n';
     return 0;
 }
