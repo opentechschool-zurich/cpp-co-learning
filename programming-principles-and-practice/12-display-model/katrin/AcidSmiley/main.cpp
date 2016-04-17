0edb50d240ca1fcd0be1025a64090d74362e5518
katrin <katrin@katrin-ubu15.10>
2016-04-17 22:05:57 +0200

    old smiley-class and gitignore

--- programming-principles-and-practice/12-display-model/katrin/AcidSmiley/main.cpp
+++ programming-principles-and-practice/12-display-model/katrin/AcidSmiley/main.cpp
@@ -1,5 +1,6 @@
 #include "Simple_window.h"
 #include "Point.h"
+#include "Arc.h"
 
 using namespace Graph_lib;
 
@@ -7,76 +8,62 @@ void smiley(Simple_window &, int, int);
 
 int main() {
 	Point topLeftWindowCorner{ 100,100 };
-	Simple_window simple_win{ topLeftWindowCorner, 600, 420, "AcidCanvas" };
+    int windowWidth = 600, windosHeight = 420;   
+	Simple_window simple_win{ topLeftWindowCorner, windowWidth, windosHeight, ":: smile ::" };
 	
-	smiley(simple_win, 300, 200); // x, y 
+    // call window with smiley
+    // objects attached by pointer to survive call
+    int faceFromLeft = 250, faceFromTop = 100;
+	smiley(simple_win, faceFromLeft, faceFromTop);  
 
 	simple_win.wait_for_button();
 	return 0;
 }
 
 /*
-There are a couple of Gotchas with this code!
-If we have all the Circle and Ellipse variables in main() we can write
-the code like this:
-
-Graph_lib::Ellipse nose{ Point{ x,y }, 5,20 };
-win.attach(nose);
-
-But if we take this code to a function we have to consider
-the following:
-
-First we have to pass the Simple_window variable by reference 
-so that we attach our shapes to the correct window.
-
-Next if we write the code as above the variable "nose" is destroyed
-when our function ends; it is only a local variable! Since the FLTK
-system only looks for our shapes when we call wait_for_button they are
-gone and FLTK draws nothing.
-
-To create the shape objects and keep them around longer than the 
-function exists we need to use the "new" keyword. With new we can ask
-for an object to be created and we get back a pointer to it's
-memory location. We can then attach the object in that memory 
-location to the window with the * operator. I.e.
-
-Graph_lib::Ellipse *nosePointer = new Graph_lib::Ellipse { Point{ x,y }, 5,20 };
-win.attach( *nosePointer );
-
-*/
+ * Graphic classes are given by reference (trought construct them by 
+ * the keyword new). 
+ */
 void smiley(Simple_window & win, int x, int y) {
 
-    // Gesichtsgrösse
-	Circle *rimPointer = new Circle { Point{ x,y }, 150 }; // Radius
-	win.attach( *rimPointer);
-    rimPointer->set_color(Color::dark_red);
-    
-    //Rectangel-Gesicht;
+    // Gesichtsgrösse:   Rectangel-Gesicht p.885 (epub reader)
+    int width = 200, hight = 300;
+    Rectangle *face = new Rectangle { Point{x,y}, width, hight};
+    win.attach(*face);  // Wert des Pointers übergeben
+    face->set_fill_color(180);  // White = 255, Black = 0, Blue =136, Yellow = 76
+    /*// Circle: Point nennt das Zentrum des Gesichts
+    int radius = 150;
+	Circle *rimPointer = new Circle { Point{ x,y }, radius};
+	win.attach( *rimPointer);  */
 
     // Augen
-	Circle *leftEyePointer = new  Circle { Point{ x - 70,y - 9 }, 35 };
+    // top left face( -> 250, | 100)
+    int radiusEyeLeft = 25;
+	Circle *leftEyePointer = new  Circle {Point{ x+radiusEyeLeft+18, y+radiusEyeLeft+43 }, radiusEyeLeft};
 	win.attach( *leftEyePointer);
-    leftEyePointer->set_fill_color(Color::black);
-
-	Circle *rightEyePointer = new Circle { Point{ x + 50,y -50 }, 25 }; 
+    leftEyePointer->set_fill_color(129); 
+    int radiusEyeRight = 25;
+	Circle *rightEyePointer = new Circle { Point{ x+radiusEyeRight+113,y+radiusEyeRight+69 },radiusEyeRight }; 
 	win.attach( *rightEyePointer );
-    rightEyePointer->set_color(Color::black);
+    rightEyePointer->set_fill_color(30);
 
     // Nase
-	Graph_lib::Ellipse *nosePointer = new Graph_lib::Ellipse { Point{ x,y }, 5,20 };
-	win.attach( *nosePointer );
+    // top left face( -> 250, | 100), Breite = 200, Höhe = 300
+    int noseWidth = 5, noseHight = 20;
+	Graph_lib::Ellipse *nosePointer = new Graph_lib::Ellipse { Point{x+100, y+150 }, noseWidth,noseHight};
+	win.attach(*nosePointer);
+    nosePointer->set_fill_color(035);
 	
     // Mund
-    // - Klasse Arc selber implementieren
-    // - Polyline Mund ersetzen durch Arc
-    // - AcidSmile nicht als Funktion, sondern als Klasse
+    // top left face( -> 250, | 100)
 	Graph_lib::Polygon *mouthPointer = new Graph_lib::Polygon;
-	(*mouthPointer).add(Point{ x - 50,y + 60 });
-	(*mouthPointer).add(Point{ x - 25,y + 65 });
-	(*mouthPointer).add(Point{ x,y + 68 });
-	(*mouthPointer).add(Point{ x + 25,y + 65 });
-	(*mouthPointer).add(Point{ x + 50,y + 60 });
+	(*mouthPointer).add(Point{ x+50,y + 200 });
+	(*mouthPointer).add(Point{ x+75,y + 205 });
+	(*mouthPointer).add(Point{ x+100,y + 208 });
+	(*mouthPointer).add(Point{ x + 125,y + 205 });
+	(*mouthPointer).add(Point{ x + 150,y + 200 });
 	win.attach(*mouthPointer);
+    mouthPointer->set_fill_color(Color::dark_red);
 
 	return;
 }
