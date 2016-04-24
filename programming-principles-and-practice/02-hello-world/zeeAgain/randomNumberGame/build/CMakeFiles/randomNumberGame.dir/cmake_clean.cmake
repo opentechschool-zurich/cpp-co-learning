7e7dfad4f5dfdfadb04412083f78ab3461f76370
zeexia <zee@zeexia.ch>
2016-04-25 00:53:18 +0200

    try again

---
+++ programming-principles-and-practice/02-hello-world/zeeAgain/randomNumberGame/build/CMakeFiles/randomNumberGame.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/randomNumberGame.dir/main.cpp.o"
+  "randomNumberGame.pdb"
+  "randomNumberGame"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/randomNumberGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
