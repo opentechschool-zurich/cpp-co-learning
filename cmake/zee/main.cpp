b69ebd03110275393144a46b77be0f5340389d17
zeexia <zee@zeexia.ch>
2016-05-13 19:24:42 +0200

    restore deleted files and load my files up

---
+++ cmake/zee/main.cpp
@@ -0,0 +1,14 @@
+// This program outputs the message "Hello World!"
+  
+  #include <iostream>
+  #include <string>
+  #include <cstdlib>
+  #include <fstream>
+
+  using namespace std;
+
+  int main() 
+  {
+    cout << "Hello World!\n";    
+    return 0;
+  }
