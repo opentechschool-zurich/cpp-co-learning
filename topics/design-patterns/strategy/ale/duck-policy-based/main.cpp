5fdab2d86a9ea965148ad39e6bca1ddf7f249c00
Ale Rimoldi <ale@graphicslab.org>
2016-05-25 08:25:04 +0200

    add the wikipedia sample for policy based design (the c++ strategy pattern) and try it out with the duck: it compiles but segment faults

---
+++ topics/design-patterns/strategy/ale/duck-policy-based/main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <string>
+ 
+template <typename QuackPolicy, typename FlyPolicy>
+class Duck : private QuackPolicy, private FlyPolicy
+{
+    using QuackPolicy::quack;
+    using FlyPolicy::fly;
+ 
+public:
+    // Behaviour method
+    void performQuack() const
+    {
+        quack();
+    }
+    void performFly() const
+    {
+        fly();
+    }
+};
+ 
+class QuackPolicyQuack
+{
+protected:
+    void quack() const
+    {
+        std::cout << "quack" << std::endl;
+    }
+};
+ 
+class QuackPolicySqueek
+{
+protected:
+    void quack() const
+    {
+        std::cout << "squeek" << std::endl;
+    }
+};
+ 
+class FlyPolicyWithWings
+{
+protected:
+    std::string fly() const
+    {
+        std::cout << "flap-flap-flap" << std::endl;
+    }
+};
+ 
+class FlyPolicyNoWay
+{
+protected:
+    std::string fly() const
+    {
+        std::cout << ".............." << std::endl;
+    }
+};
+ 
+int main()
+{
+    typedef Duck<QuackPolicyQuack, FlyPolicyWithWings> RedheadDuck;
+ 
+    RedheadDuck readheadDuck;
+    readheadDuck.performFly();
+    readheadDuck.performQuack();
+ 
+
+    typedef Duck<QuackPolicySqueek, FlyPolicyNoWay> RubberDuck;
+ 
+    RubberDuck rubberDuck;
+    rubberDuck.performFly();
+    rubberDuck.performQuack();
+}
