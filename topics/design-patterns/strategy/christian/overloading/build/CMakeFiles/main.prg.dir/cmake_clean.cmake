c716c17da1f04e6b597de05910f71e811398022e
Christian Kascha <christian.kascha@posteo.de>
2016-05-26 19:44:02 +0200

    Move my stuff into the new topic directories.

---
+++ topics/design-patterns/strategy/christian/overloading/build/CMakeFiles/main.prg.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.prg.dir/main.cpp.o"
+  "main.prg.pdb"
+  "main.prg"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.prg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
