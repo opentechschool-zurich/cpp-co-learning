f4746d85fae42f9223b856d275c62c4ee097f1ff
Richard Eigenmann <richard.eigenmann@gmail.com>
2016-07-14 00:05:23 +0200

    Starting on a GUI with the Observer Pattern

---
+++ topics/wxwidgets/Richard/ObserverGUI/MyApp.cpp
@@ -0,0 +1,45 @@
+#include "wx/wx.h"
+#include "wx/sizer.h"
+#include "wx/app.h"
+#include "Definitions.h"
+#include "MyFrame.h"
+
+/**
+ * Defines a wxDidgets application
+ */
+class MyApp: public wxApp {
+    bool OnInit();
+
+    wxFrame *myFrame;
+};
+
+IMPLEMENT_APP(MyApp)
+
+bool MyApp::OnInit() {
+    myFrame = new MyFrame((wxFrame *)NULL, -1,  wxT("myFrame"), wxPoint(50,50), wxSize(800,600));
+    myFrame->SetAutoLayout(true);
+    myFrame->Show();
+
+    return true;
+}
+
+
+//BEGIN_EVENT_TABLE(BasicDrawPane, wxPanel)
+//EVT_LEFT_UP(BasicDrawPane::mouseClicked)
+//EVT_TIMER(TIMER_ID, BasicDrawPane::OnTimer)
+// some useful events
+/*
+ EVT_MOTION(BasicDrawPane::mouseMoved)
+ EVT_LEFT_DOWN(BasicDrawPane::mouseDown)
+ EVT_LEFT_UP(BasicDrawPane::mouseReleased)
+ EVT_RIGHT_DOWN(BasicDrawPane::rightClick)
+ EVT_LEAVE_WINDOW(BasicDrawPane::mouseLeftWindow)
+ EVT_KEY_DOWN(BasicDrawPane::keyPressed)
+ EVT_KEY_UP(BasicDrawPane::keyReleased)
+ EVT_MOUSEWHEEL(BasicDrawPane::mouseWheelMoved)
+ */
+
+// catch paint events
+//EVT_PAINT(BasicDrawPane::paintEvent)
+
+//END_EVENT_TABLE()
