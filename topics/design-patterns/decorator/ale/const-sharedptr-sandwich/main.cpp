12bacca3e230cc0ab2db5684dc0cb7e55b32cbe9
ale rimoldi <ale@graphicslab.org>
2016-08-09 20:06:12 +0200

    get the decorator sandwich example with shared pointers to work

--- topics/design-patterns/decorator/ale/const-sharedptr-sandwich/main.cpp
+++ topics/design-patterns/decorator/ale/const-sharedptr-sandwich/main.cpp
@@ -11,28 +11,30 @@ using namespace std;
 
 int main()
 {
-
+    {
     shared_ptr<Ingredient> toast = make_shared<WheatBread>();
     toast = make_shared<CheeseDecorator>(toast);
     toast = make_shared<HamDecorator>(toast);
+    cout << "toast:" << endl;
     cout<< toast->getDescription() << ": " << toast->getCost() << endl;
+    }
+    cout << " toast deleted" << endl;
 
-    cout << "delete toast" << endl;
-    delete toast;
-
-    /*
     shared_ptr<Ingredient> sandwichVerdure = make_shared<WholeGrainBread>();
-    sandwichVerdure = new CheeseDecorator(sandwichVerdure);
-    sandwichVerdure = new VegDecorator(sandwichVerdure);
+    sandwichVerdure = make_shared<CheeseDecorator>(sandwichVerdure);
+    sandwichVerdure = make_shared<VegDecorator>(sandwichVerdure);
 
-    cout<< sandwichVerdure->getDescription() << ": " << sandwichVerdure->getCost() << endl;
 
-    shared_ptr<Ingredient> sandwichRoyal = make_shared<HamDecorator>(sandwichRoyal);
+    {
+    shared_ptr<Ingredient> sandwichRoyal = make_shared<HamDecorator>(sandwichVerdure);
+    cout << "sandwichRoyal:" << endl;
     cout<< sandwichRoyal->getDescription() << ": " << sandwichRoyal->getCost() << endl;
+    }
+    cout << "sandwichRoyal deleted" << endl;
 
-    cout << "delete sandwichRoyal" << endl;
-    delete sandwichRoyal;
-    cout << "delete sandwichVerdure" << endl;
-    delete sandwichVerdure;
-    */
+    cout << "sandwichVerdure:" << endl;
+    cout<< sandwichVerdure->getDescription() << ": " << sandwichVerdure->getCost() << endl;
+    cout<< (*sandwichVerdure).getDescription() << ": " << sandwichVerdure->getCost() << endl;
+
+    cout << "sandwichVerdure will be deleted at the of the program" << endl;
 }
