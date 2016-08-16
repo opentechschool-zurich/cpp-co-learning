dbb2e10d3a3561afc6c8e62d67e5b231157001dd
Sondermann <sonder.mann@posteo.org>
2016-08-16 09:46:49 +0200

    Small changes observer pattern.

--- topics/design-patterns/observer/christian/hfbsp/main.cpp
+++ topics/design-patterns/observer/christian/hfbsp/main.cpp
@@ -2,11 +2,21 @@
 #include<iostream>
 #include"interfaces.h"
 #include"weatherdisplays.h"
+
 using namespace std; 
 
 int main()
 {
-  cout << "Hello" << endl; 
 
+  WeatherData wdata;
+  wdata.setNewValues(23.0, 45.0, 40.0);
+
+  CurrentConditionsDisplay currentDisplay(wdata);
+  
+  wdata.setNewValues(23.0, 30.0, 46.0);
+  wdata.setNewValues(23.0, 35.0, 46.0);
+
+  
   return 0;
 }
+
--- topics/design-patterns/observer/christian/hfbsp/weatherdisplays.h
+++ topics/design-patterns/observer/christian/hfbsp/weatherdisplays.h
@@ -1,17 +1,55 @@
+#pragma once
+#include<iostream>
+#include<vector>
+#include"interfaces.h"
 
+using namespace std;
 
 class WeatherData : public Subject
 {
 public:
-  WeatherData();
-  virtual ~WeatherData();
-  void registerObserver(Observer* o);
-  void removeObserver(Observer* o);
-  void notifyObserver();
+
+  void registerObserver(Observer*);
+  void removeObserver(Observer*);
+  void notifyObservers();
   void setNewValues(float, float, float); 
+
 private:
   double temperature;
   double humidity;
   double pressure;
-  vector<*Observer> observer_vec; 
+  vector<Observer*> observer_vec; 
 };
+
+
+class CurrentConditionsDisplay : public Observer, public DisplayElement
+{
+public:
+  CurrentConditionsDisplay(WeatherData weatherD){
+    wD = weatherD;
+    wD.registerObserver(this); 
+  }
+
+  void display(){
+    cout << "Current condtions: " << temperature << "C degress and "
+         << humidity << "% humidity" << endl; 
+  }
+  
+  void update(double t, double h, double p){
+
+    temperature = t;
+    humidity = h;
+    cout << "updated" << endl; 
+    display(); 
+  }
+
+
+
+private:
+  double temperature;
+  double humidity;
+  WeatherData wD; 
+
+};
+
+
