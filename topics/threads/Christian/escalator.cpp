d27ecfe7942135893cddb5a17a4f5eca6027ed34
Sondermann <sonder.mann@posteo.org>
2016-08-23 21:08:31 +0200

    fooled around with escalator

--- topics/threads/Christian/escalator.cpp
+++ topics/threads/Christian/escalator.cpp
@@ -1,95 +1,139 @@
-#include <thread>
 #include <iostream>
 #include <random>
 #include <chrono>
 #include <unistd.h>
+#include <thread>
+#include <mutex>
+
 
 using namespace std;
 
 namespace {
 
-std::mt19937_64 eng{std::random_device{}()};  // or seed however you want
-std::uniform_int_distribution<> dist{5, 80};
-std::uniform_int_distribution<> boolDist{0, 1};
+  std::mt19937_64 eng{std::random_device{}()};  // or seed however you want
+  std::uniform_int_distribution<> dist{5, 80};
+  std::uniform_int_distribution<> boolDist{0, 1};
 
-void sleepRandomTime() {
-  std::this_thread::sleep_for(std::chrono::milliseconds{dist(eng)});
-}
+  void sleepRandomTime() {
+    std::this_thread::sleep_for(std::chrono::milliseconds{dist(eng)});
+  }
 
-enum class Direction{
-  up, down
-};
+  enum class Direction{
+    up, down
+      };
 
-class Person
-{
-public:
-  Person(Direction d): m_d{d}{};
-  Direction m_d;
-};
-
-class Escalator
-{
-public:
-  void stepOn(Person p){
-    cout << "Person steps on." << endl;
-    if(nPersons==0){
-      d = p.m_d;
-      if (d == Direction::up)
-	cout << "Escalator going up now..." << endl;
-      else
-	cout << "Escalator going down now..." << endl;
+  class Person
+  {
+  public:
+    Person(Direction d): m_d{d}{};
+    Direction m_d;
+    void set_number(int i){
+      number = i; 
     }
-    else{
-      if (p.m_d != d) {
-	cout << "Person falling off the elevator ... ahhhhhh" << endl;
+    int number; 
+  };
+  
+
+  std::mutex access_from_above;
+  std::mutex access_from_below;
+
+
+  class Guard {
+  public:
+    explicit Guard(std::mutex& m): m{m} {m.lock();}
+    ~Guard() {m.unlock();}
+  private:
+    std::mutex& m;
+  };
+
+
+  class Escalator
+  {
+  public:
+    void stepOn(Person p){
+      cout << "Person "<< p.number <<" steps on." << endl;
+      if(nPersons==0){
+	d = p.m_d;
+	if (d == Direction::up)
+	  cout << "Escalator going up now..."; 
+	else
+	  cout << "Escalator going down now..."; 
       }
+      else{
+	if (p.m_d != d) {
+	  cout << "Person stumbling in opp direction." << endl;
+	}
+      }
+      nPersons++;
+      cout << "with " << nPersons << endl; 
+      usleep(400000);
     }
-    nPersons++;
+
+    void changedir(){
+      Guard g(dir_mutex); 
+    }
+
+    void stepOff(Person p){
+      cout << "Person " << p.number << " steps off" << endl;
+      nPersons--;
+    }
+    Direction d = Direction::up;
+  private:
+    int nPersons{0};
+    std::mutex dir_mutex;
+  };
+
+
+  // namespace-global instance of our escalator
+  Escalator e;
+
+  
+
+  /**
+   * Makes a Person with a random Direction
+   */
+  int PersonNumber = 0;
+  
+  Person makePerson() {
+    Person p = boolDist(eng) ? Person(Direction::up) : Person(Direction::down);
+    PersonNumber++; 
+    p.set_number(PersonNumber); 
+    return p; 
   }
-  void stepOff(){
-    cout << "Person steps off" << endl;
-    nPersons--;
+
+  /**
+   *  Function to be run from withing a thread
+   */
+  void personSteppingOnEscalator() {
+    Person  newperson = makePerson();
+
+    if (newperson.m_d == e.d) {
+      e.stepOn( newperson );
+    }
+    else {
+      e.changedir(); 
+      e.stepOn( newperson );
+    }
+    e.stepOff( newperson);
   }
-private:
-  int nPersons{0};
-  Direction d;
-};
+  
 
-
-// namespace-global instance of our escalator
-Escalator e;
-
-
-/**
-* Makes a Person with a random Direction
-*/
-Person makePerson() {
-    return boolDist(eng) ? Person(Direction::up) : Person(Direction::down);
-}
-
-/**
-*  Function to be run from withing a thread
-*/
-void personSteppingOnEscalator() {
-    e.stepOn( makePerson() );
-}
-
-class Producer {
-public:
+  class Producer {
+  public:
     Producer() {
-        thread producerThread(&Producer::producePeople, this);
-        producerThread.detach();
+      thread producerThread(&Producer::producePeople, this);
+      producerThread.detach();
     }
-private:
+  private:
     void producePeople() {
-        for ( int i=1; i<10; ++i ) {
-            thread t(&personSteppingOnEscalator);
-            t.detach();
-            this_thread::sleep_until(chrono::system_clock::now() + chrono::milliseconds(200));
-        }
-        cout << "Finished producing People\n";
+      for ( int i=1; i<10; ++i ) {
+	thread t(&personSteppingOnEscalator);
+	t.detach();
+	this_thread::sleep_until(chrono::system_clock::now() + chrono::milliseconds(200));
+      }
+      cout << "Finished producing People\n";
     }
-};
+  };
 
 } // anonymous namespace
 
