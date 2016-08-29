f1fd556406bd9ec9a95eb4532bb58984c7049da2
Richard Eigenmann <richard.eigenmann@gmail.com>
2016-08-29 16:16:18 +0200

    Escalator solution with condition_variable

--- topics/threads/Richard/Readme.md
+++ topics/threads/Richard/Readme.md
@@ -34,3 +34,105 @@ No (9)
 Yes (8)
 Yes (9)
 ```
+
+
+# Escalator problem
+
+## Same as Unisex Toilet problem or Unisex shower problem
+
+We have a Mall with two escalator where one is undergoing maintenance.
+People arrive on threads and want to go up or down. If nobody is on the
+escalator they are allowed to set the direction and step on. They stay
+varying amounts of time on the escalator: Some just step on and wait till
+they reach the other end. Others walk thereby reducing the time spent. Some
+children walk in the other direction and take longer. There is only space for
+3 people to be on the escalator at any time. People wanting to go in the other
+direction must wait till nobody is on the elevator before they can change
+the direction. If 3 people are on the escalator people need to wait before
+someone steps off before they are allowed to step on.
+
+The key functionality is happening in the Escalator class:
+
+```cpp
+Direction escalatorDirection = Direction::idle;
+std::mutex mtx;
+std::condition_variable cv;
+
+/**
+* any thread can request a person to ride the escalator
+*/
+auto ride(Person p) -> void {
+  p.describe();
+
+  { // block to ensure the lock is unlocked event when an exception occurs
+    std::unique_lock<std::mutex> lck(mtx);
+    cv.wait(lck, [this, p] {
+      return (nPersons < 3 && escalatorDirection == p.intendedDirection) ||
+             nPersons == 0;
+    });
+    hopOn(p);
+  }
+  cv.notify_all();
+  usleep(800000);
+  stepOff(p);
+  cv.notify_all();
+}
+```
+
+When a person wants to ride the escalator they use the condition_variable to
+wait on the lock and atomically check and set the conditions. When something
+relevant changed the notify_all is called which tells all waiting threads to
+go and have a look at the condition and run if OK.
+
+
+```
+Escalator going idle with 0 aboard.
+Person 1 wants to ride the escalator up
+Person 1 hops on the escalator
+Person 1 is changing the direction from idle to up
+Escalator going up with 1 aboard.
+        Person 2 wants to ride the escalator up
+        Person 2 hops on the escalator
+        Escalator going up with 2 aboard.
+                Person 3 wants to ride the escalator up
+                Person 3 hops on the escalator
+                Escalator going up with 3 aboard.
+                        Person 4 wants to ride the escalator down
+Person 1 steps off the escalator
+Escalator going up with 2 aboard.
+                                Person 5 wants to ride the escalator up
+                                Person 5 hops on the escalator
+                                Escalator going up with 3 aboard.
+        Person 2 steps off the escalator
+        Escalator going up with 2 aboard.
+                                        Person 6 wants to ride the escalator down
+                Person 3 steps off the escalator
+                Escalator going up with 1 aboard.
+                                                Person 7 wants to ride the escalator down
+                                                        Person 8 wants to ride the escalator up
+                                                        Person 8 hops on the escalator
+                                                        Escalator going up with 2 aboard.
+                                Person 5 steps off the escalator
+                                Escalator going up with 1 aboard.
+                                                                Person 9 wants to ride the escalator down
+                                                        Person 8 steps off the escalator
+                                                        Escalator going up with 0 aboard.
+                        Person 4 hops on the escalator
+                        Person 4 is changing the direction from up to down
+                        Escalator going down with 1 aboard.
+                                        Person 6 hops on the escalator
+                                        Escalator going down with 2 aboard.
+                                                Person 7 hops on the escalator
+                                                Escalator going down with 3 aboard.
+                        Person 4 steps off the escalator
+                                        Person 6 steps off the escalator
+                                        Escalator going down with 1 aboard.
+                        Escalator going down with 2 aboard.
+                                                                Person 9 hops on the escalator
+                                                Person 7 steps off the escalator
+                                                                Escalator going down with 2 aboard.
+                                                Escalator going down with 1 aboard.
+                                                                Person 9 steps off the escalator
+                                                                Escalator going down with 0 aboard.
+Time ran out.
+```
--- topics/threads/Richard/escalator.cpp
+++ topics/threads/Richard/escalator.cpp
@@ -1,132 +1,190 @@
-#include <iostream>
-#include <random>
 #include <chrono>
-#include <unistd.h>
-#include <thread>
+#include <condition_variable> // std::condition_variable
+#include <iostream>
 #include <mutex>
-#include <boost/thread.hpp>
-#include <string>
+#include <random>
 #include <sstream>
+#include <string>
+#include <thread>
+#include <unistd.h>
 
 using namespace std;
 
 namespace {
 
-  std::mt19937_64 eng{std::random_device{}()};  // or seed however you want
-  std::uniform_int_distribution<> dist{5, 80};
-  std::uniform_int_distribution<> boolDist{0, 1};
+std::mt19937_64 eng{std::random_device{}()}; // or seed however you want
+std::uniform_int_distribution<> distribution{5, 80};
+std::uniform_int_distribution<> boolDist{0, 1};
 
-  void sleepRandomTime() {
-    std::this_thread::sleep_for(std::chrono::milliseconds{dist(eng)});
-  }
+void sleepRandomTime() {
+  std::this_thread::sleep_for(std::chrono::milliseconds{distribution(eng)});
+}
 
-  enum class Direction {up, down};
+enum class Direction { up, down, idle };
 
-  class Person  {
-  public:
-    Person(Direction d): m_d{d} , number(++PersonNumber){
-        //PersonNumber++;
-        //number = PersonNumber;
-        //number = 1;
-    };
-    Direction m_d;
-    int number;
-    static int PersonNumber;
+/**
+* returns a description for the Direction enum*/
+auto getDirectonString(Direction d) -> string {
+  if (d == Direction::up)
+    return "up";
+  else if (d == Direction::down)
+    return "down";
+  else
+    return "idle";
+}
 
-    auto tabs(void) -> string {
-        std::stringstream ss;
-        for ( int i = 0; i < number; ++i)
-            ss << "\t";
-        return ss.str();
-    }
-  };
-
-  int Person::PersonNumber=0;
-
-  class Escalator  {
-  public:
-      Direction d = Direction::up;
-      auto ride(Person p) -> void {
-            string direction = p.m_d == Direction::up ? "up" : "down";
-            cout << p.tabs() << "Person "  << p.number << " wants to ride the escalator " << direction << endl;
-            if ( p.m_d != d ) {
-                cout << p.tabs() <<"We need to change the direction of the elevator\n";
-                //boost::upgrade_lock<boost::shared_mutex> lock(escalator_moving);
-                //boost::upgrade_to_unique_lock<boost::shared_mutex> uniqueLock(escalator_moving);
-                boost::unique_lock< boost::shared_mutex > lock(escalator_moving);
-                cout << p.tabs() <<"Person "  << p.number << " changed the direction to " << direction << endl;
-                d = p.m_d;
-            }
-            boost::shared_lock<boost::shared_mutex> lock( escalator_moving );
-            cout << p.tabs() <<"Person "  << p.number << " hops on the escalator\n" ;
-            if (p.m_d != d) {
-	             cout << p.tabs() <<"Person " << p.number << " stumbling in opp direction." << endl;
-	        }
-            nPersons++;
-            print_escalator_state(p.tabs());
-            usleep(800000);
-            cout << p.tabs() <<"Person "  << p.number << " steps off the escalator " << endl;
-            nPersons--;
-            print_escalator_state(p.tabs());
-            //cout << p.tabs() <<"Person "  << p.number << " shared lock going out of scope" << endl;
-        }
-
-
-    auto print_escalator_state( string tabs ) -> void {
-        string direction = d == Direction::up ? "up" : "down";
-        cout << tabs << "Escalator going " << direction  << " with " << nPersons << " aboard.\n";
-    }
-  private:
-    int nPersons{0};
-    boost::shared_mutex escalator_moving;
-  };
-
-
-
-  // namespace-global instance of our escalator
-  Escalator e;
-
-
+/**
+* Person class that holds the direction the person intents to travel on the
+* escalator
+*/
+class Person {
+public:
+  Person(Direction intendedDirection)
+      : intendedDirection{intendedDirection}, number(++totalPersons){};
+  Direction intendedDirection;
+  int number;              // the unique number of the Person
+  static int totalPersons; // how many People created so far
 
   /**
-   * Makes a Person with a random Direction
-   */
-  Person makePerson() {
-    Person p = boolDist(eng) ? Person(Direction::up) : Person(Direction::down);
-
-    return p;
+  * returns a string of tabs that allows the person output to be indented by
+  * the member var number to facilitate understanding of which thread is
+  * doing what.
+  */
+  auto tabs(void) -> string {
+    stringstream s;
+    for (int i = 1; i < number; ++i)
+      s << "\t";
+    return s.str();
   }
 
   /**
-   *  Function to be run from withing a thread
-   */
-  void personSteppingOnEscalator() {
-    Person  newperson = makePerson();
-    e.ride( newperson );
+  * describes the person's intent
+  */
+  auto describe() -> void {
+    stringstream s;
+    s << tabs() << "Person " << number << " wants to ride the escalator "
+      << getDirectonString(intendedDirection) << endl;
+    cout << s.str();
+  }
+};
+
+// static initialiser (Can't be initialized in the class)
+int Person::totalPersons = 0;
+
+/**
+ * Makes a Person with a random Direction
+ */
+auto makeRandomPerson() -> Person {
+  Person p = boolDist(eng) ? Person(Direction::up) : Person(Direction::down);
+  return p;
+};
+
+class Escalator {
+public:
+  Direction escalatorDirection = Direction::idle;
+  std::mutex mtx;
+  std::condition_variable cv;
+
+  /**
+  * any thread can request a person to ride the escalator
+  */
+  auto ride(Person p) -> void {
+    p.describe();
+
+    { // block to ensure the lock is unlocked event when an exception occurs
+      std::unique_lock<std::mutex> lck(mtx);
+      cv.wait(lck, [this, p] {
+        return (nPersons < 3 && escalatorDirection == p.intendedDirection) ||
+               nPersons == 0;
+      });
+      hopOn(p);
+    }
+    cv.notify_all();
+    usleep(800000);
+    stepOff(p);
+    cv.notify_all();
   }
 
+  auto printEscalatorState(string tabs) -> void {
+    string direction = escalatorDirection == Direction::up ? "up" : "down";
+    stringstream s;
+    s << tabs << "Escalator going " << getDirectonString(escalatorDirection)
+      << " with " << nPersons << " aboard.\n";
+    cout << s.str();
+  }
 
-  class Producer {
-  public:
-    Producer() {
-      thread producerThread(&Producer::producePeople, this);
-      producerThread.detach();
+private:
+  int nPersons{0};
+
+  auto hopOn(Person p) -> void {
+    stringstream s;
+    s << p.tabs() << "Person " << p.number << " hops on the escalator\n";
+    cout << s.str();
+
+    if (nPersons == 0 && p.intendedDirection != escalatorDirection) {
+      stringstream change;
+      change << p.tabs() << "Person " << p.number
+             << " is changing the direction from "
+             << getDirectonString(escalatorDirection) << " to "
+             << getDirectonString(p.intendedDirection) << endl;
+      cout << change.str();
+      escalatorDirection = p.intendedDirection;
     }
-  private:
-    void producePeople() {
-      for ( int i=1; i<10; ++i ) {
-	thread t(&personSteppingOnEscalator);
-	t.detach();
-	this_thread::sleep_until(chrono::system_clock::now() + chrono::milliseconds(200));
-      }
-      cout << "Finished producing People\n";
+
+    if (p.intendedDirection != escalatorDirection) {
+      stringstream fall;
+      fall << p.tabs() << "Person " << p.number << " falls and hurts themself."
+           << endl;
+      cout << fall.str();
     }
-  };
+    nPersons++;
+    printEscalatorState(p.tabs());
+  }
+
+  auto stepOff(Person p) -> void {
+    stringstream s;
+    s << p.tabs() << "Person " << p.number << " steps off the escalator "
+      << endl;
+    cout << s.str();
+    nPersons--;
+    printEscalatorState(p.tabs());
+  }
+};
+
+// namespace-global instance of our escalator
+Escalator e;
+
+/**
+ *  Function to be run from within a thread
+ */
+void personThread() {
+  Person newperson = makeRandomPerson();
+  e.ride(newperson);
+}
+
+class Producer {
+public:
+  Producer() {
+    thread producerThread(&Producer::producePeopleThreads, this);
+    producerThread.join();
+  }
+
+private:
+  void producePeopleThreads() {
+    e.printEscalatorState("");
+    for (int i = 1; i < 10; ++i) {
+      thread t(&personThread);
+      t.detach();
+      this_thread::sleep_until(chrono::system_clock::now() +
+                               chrono::milliseconds(200));
+    }
+  }
+};
 
 } // anonymous namespace
 
 int main() {
-  Producer producer {};
+  Producer producer{};
   usleep(10000000);
   cout << "Time ran out.\n";
 }
