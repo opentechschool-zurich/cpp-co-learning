9c02016f868f19ec397d4e1bb51f30efb82e42a1
ale rimoldi <ale@graphicslab.org>
2016-12-16 15:33:51 +0100

    implement the falling boxes examples

---
+++ topics/games/monkey-keg/ale/boxes/README.md
@@ -0,0 +1,12 @@
+# Falling boxes
+
+Use SFML and Box2d to draw boxes that fall down.
+
+https://veendeta.wordpress.com/2012/02/16/tutorial-getting-started-with-box2d/
+
+- Modifiy the sample to match the `init()`, `update()`, `render()` model.
+- Use the newer SFML functions.
+
+Status:
+
+- It shows the falling rectangles but on my computer the area borders are not correct (and the boxes fall down down down...)
---
+++ topics/games/monkey-keg/ale/boxes/src/game.h
@@ -0,0 +1,38 @@
+#ifndef MONKEYKEG_GAME_H
+
+#include <SFML/Graphics.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/System/Time.hpp>
+#include <Box2D/Box2D.h>
+
+namespace MonkeyKeg {
+    class Game
+    {
+    public:
+        Game();
+
+        void init();
+        void update(const float dt);
+        void render();
+        void run();
+
+    private:
+        sf::RenderWindow window;
+        // sf::CircleShape shape;
+        sf::Clock deltaClock;
+
+		/** for converting between pixel and real-world coordinates*/
+		static constexpr float scale = 30.f;
+
+        b2Vec2 gravity;
+        b2World world;
+
+		sf::Texture groundTexture;
+		sf::Texture boxTexture;
+
+        void createBox(int x, int y);
+        void createGround(float X, float Y);
+    };
+}
+
+#endif
