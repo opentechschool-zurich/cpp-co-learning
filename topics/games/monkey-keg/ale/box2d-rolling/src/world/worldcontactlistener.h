8bc714f9fa06d5e4fd24cdb3ef9e4229f645633c
ale rimoldi <ale@graphicslab.org>
2017-01-24 20:27:12 +0100

    add a first rolling barrel

---
+++ topics/games/monkey-keg/ale/box2d-rolling/src/world/worldcontactlistener.h
@@ -0,0 +1,19 @@
+#ifndef MONKEYKEG_GAME_WORLD_WORLDCONTACTLISTENER_H
+#define MONKEYKEG_GAME_WORLD_WORLDCONTACTLISTENER_H
+
+#include <SFML/Graphics.hpp>
+#include <Box2D/Box2D.h>
+
+namespace Box2DRolling {
+    /**
+     * @brief Notify `worlditem`s of contacts
+     */
+    class WorldContactListener : public b2ContactListener
+    {
+        public:
+            void BeginContact(b2Contact* contact);
+            void EndContact(b2Contact* contact);
+    };
+}
+
+#endif
