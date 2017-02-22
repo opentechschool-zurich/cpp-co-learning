8ac779f0d6b6b89ebefbbe581f55c3b633858248
ale rimoldi <ale@graphicslab.org>
2017-02-22 21:09:15 +0100

    add the constructor with start and end coordinates

--- topics/games/monkey-keg/ale/monkey-keg/src/world/ground.cpp
+++ topics/games/monkey-keg/ale/monkey-keg/src/world/ground.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+#include <cmath>
 #include "world/ground.h"
 #include "game.h"
 
@@ -7,6 +9,9 @@
  */
 
 namespace MonkeyKeg {
+    /*
+     * @brief create a floor/ground with start coordinate, length and angle
+     */
     Ground::Ground(b2World* world, int x, int y, int length, int angle) :
         length(length)
     {
@@ -27,26 +32,33 @@ namespace MonkeyKeg {
     }
 
     /**
-     * TODO: calculate length and angle according to the start and end points
+     * @brief create a floor/ground with start and end pixel coordinates
      */
     Ground::Ground(b2World* world, b2Vec2 start, b2Vec2 end)
     {
-        /*
+        std::cout << start.x << std::endl;
+        length = std::sqrt(std::pow(end.x - start.x, 2) + std::pow(end.y - start.y, 2));
+        float angle = std::atan2(end.y - start.y, end.x - start.x);
+        std::cout << "length: " << length << std::endl;
+        // std::cout << "angle: " << angle * radtodeg << std::endl;
+        std::cout << "angle: " << angle << std::endl;
+
         b2BodyDef BodyDef;
-        BodyDef.position = b2Vec2(x/scale, y/scale);
+        // TODO: probably, we have to use the middle point
+        BodyDef.position = b2Vec2(start.x/scale, start.y/scale);
         BodyDef.type = b2_staticBody;
         b2Body* Body = world->CreateBody(&BodyDef);
 
         Body->SetUserData(this);
 
         b2PolygonShape Shape;
-        Body->SetTransform(Body->GetWorldCenter(), angle * degtorad);
+        Body->SetTransform(Body->GetWorldCenter(), angle);
+        // TODO: can SetAsBox directly use x/y?
         Shape.SetAsBox((length/2)/scale, (height/2)/scale);
         b2FixtureDef FixtureDef;
         FixtureDef.density = 0.f;
         FixtureDef.shape = &Shape;
         Body->CreateFixture(&FixtureDef);
-        */
     }
 
     void Ground::render(sf::RenderWindow* window, b2Body* body)
