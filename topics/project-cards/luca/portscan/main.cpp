ae1c9b1a09ad511452829d26e69b1a6691bf724d
Luca Marchesi <skiat@skiat.ch>
2017-06-20 19:42:40 +0200

    project setup and basic steps in comments

---
+++ topics/project-cards/luca/portscan/main.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <regex>
+
+int main()
+{
+	// 1. get values from user
+
+		// 1.1 ip range
+		// 1.2 port range
+
+	
+	// 2. prepare list of targets
+
+	// 3. scan one target
+	
+	// 4. scan the ports
+}
